1a5f55be7132c3eab8aa7cea09dfa6b8e7dcf1c2
Stefan Wahren <wahrenst@gmx.net>
2024-07-14 16:20:38 +0200

    Revert "pmdomain: raspberrypi-power: Avoid powering down USB"
    This reverts commit 6240930f615296e2cd16fa4c3ed74a1a144031c5.

--- drivers/pmdomain/bcm/raspberrypi-power.c
+++ drivers/pmdomain/bcm/raspberrypi-power.c
@@ -86,14 +86,7 @@ static void rpi_common_init_power_domain(struct rpi_power_domains *rpi_domains,
 
 	dom->base.name = name;
 	dom->base.power_on = rpi_domain_on;
-
-	/*
-	 * During supend to idle any request to power off the USB domain
-	 * leads to a timeout. As a temporary workaround don't register
-	 * the relevant power off handler.
-	 */
-	if (strcmp("USB", name))
-		dom->base.power_off = rpi_domain_off;
+	dom->base.power_off = rpi_domain_off;
 
 	/*
 	 * Treat all power domains as off at boot.
