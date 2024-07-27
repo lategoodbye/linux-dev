6e9ecacdb9250e6a3a7a48eea6168b07a6d47f4a
Stefan Wahren <wahrenst@gmx.net>
2024-07-27 11:29:27 +0200

    drm/vc4: v3d: simplify clock retrieval
    Common pattern of handling deferred probe can be simplified with
    dev_err_probe() and devm_clk_get_optional(). This results in much
    less code.
    
    Signed-off-by: Stefan Wahren <wahrenst@gmx.net>

--- drivers/gpu/drm/vc4/vc4_v3d.c
+++ drivers/gpu/drm/vc4/vc4_v3d.c
@@ -441,20 +441,11 @@ static int vc4_v3d_bind(struct device *dev, struct device *master, void *data)
 	vc4->v3d = v3d;
 	v3d->vc4 = vc4;
 
-	v3d->clk = devm_clk_get(dev, NULL);
+	v3d->clk = devm_clk_get_optional(dev, NULL);
 	if (IS_ERR(v3d->clk)) {
 		int ret = PTR_ERR(v3d->clk);
 
-		if (ret == -ENOENT) {
-			/* bcm2835 didn't have a clock reference in the DT. */
-			ret = 0;
-			v3d->clk = NULL;
-		} else {
-			if (ret != -EPROBE_DEFER)
-				dev_err(dev, "Failed to get V3D clock: %d\n",
-					ret);
-			return ret;
-		}
+		return dev_err_probe(dev, ret, "Failed to get V3D clock\n");
 	}
 
 	ret = platform_get_irq(pdev, 0);
